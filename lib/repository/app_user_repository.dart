f90fc53156cd87a5d58f7e530cad037810b1ddc2
onurkaynar <onur.kaynar@hotmail.com>
2020-10-12 21:36:15 +0300

    225

---
+++ lib/repository/app_user_repository.dart
@@ -0,0 +1,45 @@
+import 'package:stok/locator.dart';
+import 'package:stok/model/app_user_model.dart';
+import 'package:stok/services/auth_base.dart';
+import 'package:stok/services/fake_auth_services.dart';
+import 'package:stok/services/firebase_auth_service.dart';
+
+enum AppMode {DEBUG,RELEASE}
+
+class AppUserRepository implements AuthBase{
+
+  FirebaseAuthService _firebaseAuthService=locator<FirebaseAuthService>();
+  FakeAuthServices _fakeAuthServices=locator<FakeAuthServices>();
+
+  AppMode appMode=AppMode.DEBUG;
+
+
+  @override
+  Future<AppUser> currentUser() async {
+  if(appMode==AppMode.DEBUG){
+    return await _fakeAuthServices.currentUser();
+  }else{
+    return await _firebaseAuthService.currentUser();
+  }
+  }
+
+  @override
+  Future<AppUser> signInAnonymously()async {
+    if(appMode==AppMode.DEBUG){
+      return await _fakeAuthServices.signInAnonymously();
+    }else{
+      return await _firebaseAuthService.signInAnonymously();
+    }
+  }
+
+  @override
+  Future<bool> signOut()async {
+    if(appMode==AppMode.DEBUG){
+      return await _fakeAuthServices.signOut();
+    }else{
+      return await _firebaseAuthService.signOut();
+    }
+  }
+
+
+}
