4e8e64c3aa0e4f82c6bce38cde1d7b7a9335dff1
onurkaynar <onur.kaynar@hotmail.com>
2020-10-31 19:05:07 +0300

    281

--- lib/app/kullanicilar.dart
+++ lib/app/kullanicilar.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stok/app/konusma_page.dart';
@@ -10,103 +11,121 @@ class KullanicilarPage extends StatefulWidget {
 }
 
 class _KullanicilarPageState extends State<KullanicilarPage> {
+  List<AppUser> _tumKullanicilar = [];
+  bool _isLoading = false;
+  bool _hasMore = true;
+  int _getirilecekElemanSayisi = 10;
+  AppUser _enSonGetirilenUser;
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUser(_enSonGetirilenUser);
+    _scrollController.addListener(() {
+
+      if(_scrollController.position.atEdge){
+        if(_scrollController.position==0){
+
+        }else{
+          getUser(_enSonGetirilenUser);
+        }
+      }
+
+    });
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     final _appUserModel = Provider.of<AppUserViewModel>(context, listen: false);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Kullanıcılar"),
-        ),
-        body: FutureBuilder<List<AppUser>>(
-          future: _appUserModel.getAllUsers(),
-          builder: (context, sonuc) {
-            if (sonuc.hasData) {
-              var tumKullanicilar = sonuc.data;
-              if (tumKullanicilar.length - 1 > 0) {
-                return RefreshIndicator(
-                  onRefresh: _kullanicilarListesiniYenile,
-                  child: ListView.builder(
-                    itemCount: tumKullanicilar.length,
-                    itemBuilder: (context, index) {
-                      var oAnKiUser = sonuc.data[index];
-                      if (oAnKiUser.appUserID != _appUserModel.user.appUserID) {
-                        return Column(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context, rootNavigator: true).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => KonusmaPage(
-                                      currentUser: _appUserModel.user,
-                                      sohbetEdilenUser: oAnKiUser,
-                                    ),
-                                  ),
-                                );
-                              },
-                              child: ListTile(
-                                title: Text(oAnKiUser.userName),
-                                subtitle: Text(oAnKiUser.email),
-                                leading: CircleAvatar(
-                                  radius: 40,
-                                  backgroundImage:
-                                      NetworkImage(oAnKiUser.profilURL),
-                                ),
-                              ),
-                            ),
-                            Divider(
-                              thickness: 1,
-                              indent: 20,
-                              endIndent: 20,
-                            )
-                          ],
-                        );
-                      } else {
-                        return Container();
-                      }
-                    },
-                  ),
-                );
-              } else {
-                return Center(
-                  child: RefreshIndicator(
-                    onRefresh: _kullanicilarListesiniYenile,
-                    child: SingleChildScrollView(
-                      physics: AlwaysScrollableScrollPhysics(),
-                      child: Container(
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(
-                                Icons.supervised_user_circle,
-                                color: Theme.of(context).primaryColor,
-                                size: 100,
-                              ),
-                              Text(
-                                "Burada Yalnızsınız",
-                                style: TextStyle(fontSize: 20),
-                              )
-                            ],
-                          ),
-                        ),
-                        height: MediaQuery.of(context).size.height - 150,
-                      ),
-                    ),
-                  ),
-                );
-              }
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          },
-        ));
+      appBar: AppBar(
+        title: Text("Kullanıcılar"),
+        actions: [
+          FlatButton(
+              onPressed: () async {
+                await getUser(_enSonGetirilenUser);
+              },
+              child: Text("Next Users"))
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: _tumKullanicilar.length > 0
+                  ? _kullaniciListesiOlustur()
+                  : Center(
+                      child: Text("Kullanıcı Yok"),
+                    )),
+          _isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container(),
+        ],
+      ),
+    );
   }
 
-  Future<Null> _kullanicilarListesiniYenile() async {
-    setState(() {});
-    await Future.delayed(Duration(seconds: 1));
-    return null;
+  getUser(AppUser enSonGetirilenUser) async {
+    if(!_hasMore){
+      print("Kullanıcı Yok");
+      return;
+
+    }
+    if(_isLoading){
+      print("isloading true");
+      return;
+    }
+    setState(() {
+      _isLoading = true;
+    });
+    QuerySnapshot _querySnapshot;
+    if (enSonGetirilenUser == null) {
+      print("İlk defa kullanıcılar getiriliyor");
+      _querySnapshot = await FirebaseFirestore.instance
+          .collection("users")
+          .orderBy("userName")
+          .limit(_getirilecekElemanSayisi)
+          .get();
+    } else {
+      print("Sonraki kullanıcılar getiriliyor");
+      _querySnapshot = await FirebaseFirestore.instance
+          .collection("users")
+          .orderBy("userName")
+          .startAfter([enSonGetirilenUser.userName])
+          .limit(_getirilecekElemanSayisi)
+          .get();
+
+
+    }
+    if(_querySnapshot.docs.length < _getirilecekElemanSayisi){
+      _hasMore=false;
+    }
+    for (DocumentSnapshot snapshot in _querySnapshot.docs) {
+      AppUser _tekUser = AppUser.fromMap(snapshot.data());
+      _tumKullanicilar.add(_tekUser);
+      print("Getirilen user name:" + _tekUser.userName);
+    }
+    _enSonGetirilenUser = _tumKullanicilar.last;
+    print("en son getirilen user name:" + _enSonGetirilenUser.userName);
+    setState(() {
+
+    });
+    _isLoading = false;
+  }
+
+  _kullaniciListesiOlustur() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemBuilder: (context, index) {
+        return ListTile(title: Text(_tumKullanicilar[index].userName),);
+      },
+      itemCount:  _tumKullanicilar.length,
+    );
   }
 }
